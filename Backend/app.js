9ff6c080596f0874e311c808295e8e3b545becf7
Somen Mahato <somenmahato124-netizen@users.noreply.github.com>
2026-09-25 16:51:46 +0530

    Fix CORS for frontend

--- Backend/app.js
+++ Backend/app.js
@@ -14,6 +14,7 @@ const port = process.env.PORT || 8000;
 app.use(
     cors({
         origin: [
+            "http://localhost:5173",
             "http://localhost:5174",
             "https://virtual-assistant-frontend-ma0h.onrender.com"
         ],
